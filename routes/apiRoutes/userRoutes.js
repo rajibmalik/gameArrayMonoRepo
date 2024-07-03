6fe5c8db66e76b2ae3fa5a7060947faa58577744
rajibmalik <rajibmalik@hotmail.co.uk>
2024-07-03 18:05:59 +0100

    API: Get User

--- routes/apiRoutes/userRoutes.js
+++ routes/apiRoutes/userRoutes.js
@@ -1,26 +1,10 @@
 const express = require('express');
-
-const User = require('../../model/userModel');
+const userController = require('../../controllers/apiControllers/userController');
 
 const router = express.Router();
 
-router.route('/').get(async (req, res) => {
-  try {
-    const users = await User.find();
+router.route('/').get(userController.getAllUsers);
 
-    res.status(200).json({
-      status: 'success',
-      results: users.length,
-      data: {
-        users: users,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-});
+router.route('/:steamID').get(userController.getUser);
 
 module.exports = router;
