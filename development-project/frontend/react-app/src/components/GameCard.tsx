2677a979368be46724a031422de974707b46452d
rajibmalik <rajibmalik@hotmail.co.uk>
2024-07-09 17:24:41 +0100

    Added playtime for GamCard

--- development-project/frontend/react-app/src/components/GameCard.tsx
+++ development-project/frontend/react-app/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import React from "react";
 import { UserGame } from "../hooks/useUserGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { FaClock } from "react-icons/fa6";
+import { Badge } from "@chakra-ui/react";
 
 interface Props {
   game: UserGame;
@@ -11,9 +12,21 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={5} overflow={"hidden"}>
       <Image src={game.headerImage}></Image>
       <CardBody>
-        <Heading overflow={"hidden"} fontSize="2xl">
+        <Heading
+          minH={"65px"}
+          //   height={"80px"}
+          display={"flex"}
+          justifyContent={"center"}
+          overflow={"hidden"}
+          fontSize="xl"
+          textOverflow="ellipsis" // Adds ellipsis for overflow text
+        >
           {game.name}
         </Heading>
+        <HStack padding={"5px"} justifyContent={"center"}>
+          <Badge>{game.playtimeHours} hours playtime</Badge>
+          <FaClock />
+        </HStack>
       </CardBody>
     </Card>
   );
