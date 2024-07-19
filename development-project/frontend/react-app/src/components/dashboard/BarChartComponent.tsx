f485d61eb82c99e09647b325e1d6b3229f00fccb
rajibmalik <rajibmalik@hotmail.co.uk>
2024-07-19 16:32:47 +0100

    Refactor: styling / tooltip for BarChartComponent

--- development-project/frontend/react-app/src/components/dashboard/BarChartComponent.tsx
+++ development-project/frontend/react-app/src/components/dashboard/BarChartComponent.tsx
@@ -1,14 +1,12 @@
-import { Box } from "@chakra-ui/react";
-
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 import {
   BarChart,
   Bar,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
+  Label,
 } from "recharts";
 import { UserGame } from "../../hooks/useUserGames";
 
@@ -16,14 +14,68 @@ interface Props {
   userGames: UserGame[];
 }
 
-const BarChartComponent = ({ userGames }: Props) => {
-  const data = userGames.map((userGame) => ({
-    name: userGame.name,
-    playtimeHours: userGame.playtimeHours,
-  }));
+// Custom label for rendering UserGame images on the X axis
+// Props provided by recharts
+const XAxisLabel = (props: any) => {
+  const { x, y, payload } = props;
+  const imageUrl = payload.value;
 
   return (
-    <Box
+    <g transform={`translate(${x},${y})`}>
+      <image href={imageUrl} x={-50} y={-25} width={100} height={100} />
+    </g>
+  );
+};
+
+// Custom label for rendering playtime above each bar
+// Props provided by recharts
+const BarLabel = (props: any) => {
+  const { x, y, width, value } = props;
+  return (
+    <text x={x + width / 2} y={y} fill="black" textAnchor="middle" dy={-2}>
+      {`${value}`}
+    </text>
+  );
+};
+
+// Props provided by recharts
+const BarChartComponent = ({ userGames }: Props) => {
+  // Process userGames data for utilising in the BarChart
+  const data = userGames.map((userGame) => {
+    return {
+      name: userGame.name,
+      playtimeHours: userGame.playtimeHours,
+      image: userGame.headerImage,
+    };
+  });
+
+  // Custom function to render tooltip content
+  // Props provided by recharts
+  const renderTooltipContent = (props: any) => {
+    const { active, payload } = props;
+
+    if (active && payload && payload.length) {
+      const data = payload[0].payload;
+
+      // Returns a styled Chakra box with the name of the game
+      return (
+        <Box
+          bg="white"
+          p="2"
+          borderWidth={0.5}
+          borderColor={"black"}
+          textColor={"black"}
+        >
+          <Text>{data.name}</Text>
+        </Box>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <VStack
       bg="white"
       p="3"
       borderRadius="md"
@@ -31,27 +83,57 @@ const BarChartComponent = ({ userGames }: Props) => {
       width={"100%"}
       height={"100%"}
     >
+      <Heading size={"md"} color={"black"}>
+        These are your most played games
+      </Heading>
       <ResponsiveContainer width={"100%"} height={"100%"}>
         <BarChart
           width={500}
           height={300}
           data={data}
           margin={{
-            top: 5,
-            right: 30,
+            top: 10,
+            right: 20,
             left: 20,
-            bottom: 5,
+            bottom: 60,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip shared={false} trigger="click" />
-          <Legend />
-          <Bar dataKey="playtimeHours" fill="#8884d8" />
+          <XAxis dataKey="image" tick={<XAxisLabel />} interval={0}>
+            <Label
+              value="Top 10 Most Played Games"
+              offset={-55}
+              position="insideBottom"
+              style={{
+                textAnchor: "middle",
+                fill: "black",
+                fontSize: 15,
+                fontWeight: "bold",
+              }}
+            />
+          </XAxis>
+          <YAxis tick={{ fill: "black" }}>
+            <Label
+              value="Hours Played"
+              angle={-90}
+              position={"left"}
+              offset={0}
+              style={{
+                textAnchor: "middle",
+                fill: "black",
+                fontSize: 15,
+                fontWeight: "bold",
+              }}
+            />
+          </YAxis>
+          <Tooltip content={renderTooltipContent} />
+          <Bar
+            dataKey="playtimeHours"
+            fill="#8884d8"
+            label={<BarLabel />}
+          ></Bar>
         </BarChart>
       </ResponsiveContainer>
-    </Box>
+    </VStack>
   );
 };
 
--- development-project/frontend/react-app/src/components/dashboard/RadarChartComponent.tsx
+++ development-project/frontend/react-app/src/components/dashboard/RadarChartComponent.tsx
@@ -20,12 +20,8 @@ export interface topGenres {
 }
 
 const RadarChartComponent = ({ topGenres }: Props) => {
-  console.log(JSON.stringify(topGenres));
   const hours = topGenres.map((genre) => genre.totalPlaytimeHours);
   const highestPlaytime = Math.max(...hours);
-
-  console.log(hours);
-  console.log(highestPlaytime);
 
   const data = topGenres.map((genreData) => ({
     subject: genreData.genre,
@@ -44,14 +40,14 @@ const RadarChartComponent = ({ topGenres }: Props) => {
         height={"100%"}
       >
         <Heading size={"sm"} textAlign={"center"} color={"black"} pb={1}>
-          Your most played genres
+          These are your most played genres
         </Heading>
         <ResponsiveContainer width="100%" height="100%">
           <RadarChart cx="50%" cy="50%" outerRadius="70%" data={data}>
             <PolarGrid />
             <PolarAngleAxis
               dataKey="subject"
-              tick={{ fill: "black" }}
+              tick={{ fill: "black", fontWeight: "bold" }}
               tickSize={17.5}
             />
             <PolarRadiusAxis tick={false} axisLine={false} />
@@ -62,8 +58,7 @@ const RadarChartComponent = ({ topGenres }: Props) => {
               animationDuration={1000}
               name="Playtime"
               dataKey="A"
-              fill="black"
-              fillOpacity={0.6}
+              fill="#8884d8"
             />
           </RadarChart>
         </ResponsiveContainer>
