22625863277b22914d2be7ac0b9d419710b5d97c
rajibmalik <rajibmalik@hotmail.co.uk>
2024-07-28 15:56:13 +0100

    Testing: gameController fetching games

--- development-project/backendApplication/__tests__/unit/api/gameController.test.js
+++ development-project/backendApplication/__tests__/unit/api/gameController.test.js
@@ -29,103 +29,24 @@ describe('gameController', () => {
     next = jest.fn();
   });
 
-  it('should create one new UserGame ', async () => {
-    const req = {
-      user: { steamID: '23456789123456789' },
-      usergames: [{ appid: '1', playtime: 100 }],
-    };
+  it('should get all of the users Games', async () => {
+    const req = {};
+    await gameController.getGames(req, res);
 
-    await userGameController.createAndUpdateUserGames(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    const userGames = await UserGame.find({
-      steamid: '23456789123456789',
-    });
-
-    expect(userGames).toHaveLength(1);
-    expect(userGames[0].appid).toBe('1');
-    expect(userGames[0].steamid).toBe('23456789123456789');
-    expect(userGames[0].playtime).toBe(100);
+    const responseData = res.json.mock.calls[0][0];
+    expect(responseData.status).toBe('success');
+    expect(responseData.results).toBe(6);
   });
 
-  it('should create three new UserGame ', async () => {
-    const req = {
-      user: { steamID: '23456789123456789' },
-      usergames: [
-        { appid: '1', playtime: 50 },
-        { appid: '2', playtime: 75 },
-        { appid: '3', playtime: 100 },
-      ],
-    };
+  it('should get the Game requested', async () => {
+    const req = { params: { appid: '1' } };
+    await gameController.getGame(req, res);
 
-    await userGamesController.createAndUpdateUserGames(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    const userGames = await UserGame.find({
-      steamid: '23456789123456789',
-    });
-
-    expect(userGames).toHaveLength(3);
-    expect(userGames[0].appid).toBe('1');
-    expect(userGames[0].steamid).toBe('23456789123456789');
-    expect(userGames[0].playtime).toBe(50);
-  });
-
-  it('should update one existing new UserGame ', async () => {
-    const req = {
-      user: { steamID: '12356789123456789' },
-      usergames: [{ appid: '1', playtime: 10000 }],
-    };
-
-    await userGamesController.createAndUpdateUserGames(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    const userGame = await UserGame.findOne({
-      steamid: '12356789123456789',
-      appid: '1',
-    });
-
-    expect(userGame.appid).toBe('1');
-    expect(userGame.steamid).toBe('12356789123456789');
-    expect(userGame.playtime).toBe(10000);
-  });
-  it('should not create a non existing game', async () => {
-    const req = {
-      user: { steamID: '12356789123456789' },
-      usergames: [{ appid: '100', playtime: 100 }],
-    };
-
-    await userGamesController.createAndUpdateUserGames(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    const userGame = await UserGame.findOne({
-      steamid: '12356789123456789',
-      appid: '100',
-    });
-
-    expect(userGame).toBeNull();
-  });
-  it('should only create the existing games ', async () => {
-    const req = {
-      user: { steamID: '12356789123456789' },
-      usergames: [
-        { appid: '1', playtime: 10000 },
-        { appid: '2000', playtime: 50 },
-      ],
-    };
-
-    await userGamesController.createAndUpdateUserGames(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    const userGames = await UserGame.find({
-      steamid: '12356789123456789',
-    });
-
-    expect(userGames).toHaveLength(1);
+    const responseData = res.json.mock.calls[0][0];
+    expect(responseData.status).toBe('success');
+    expect(responseData.data.game).toBeDefined();
+    expect(responseData.data.game.appid).toBe('1');
+    expect(responseData.data.game.name).toBe('gameOne');
+    expect(responseData.data.game.genres).toEqual(['Action', 'RPG']);
   });
 });
