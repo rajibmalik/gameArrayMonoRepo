6fabdee8fc681d48ac4bea788349f1fb7f319ac5
rajibmalik <rajibmalik@hotmail.co.uk>
2024-07-28 22:13:05 +0100

    Testing: Homepage page component

---
+++ development-project/frontend/react-app/src/tests/components/homepage/Homepage.test.tsx
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Homepage from "../../../pages/Homepage";
+
+describe("Homepage", () => {
+  it("renders all components with expected text", async () => {
+    render(<Homepage />);
+
+    // Headers
+    expect(screen.getByText("WELCOME")).toBeInTheDocument();
+    expect(screen.getByText("TO ACCESS YOUR")).toBeInTheDocument();
+
+    // InfoContainer components
+    expect(screen.getByText("LIBRARY")).toBeInTheDocument();
+    expect(screen.getByText("DASHBOARD")).toBeInTheDocument();
+
+    // AuthButton
+    expect(screen.getByText("LOGIN WITH STEAM")).toBeInTheDocument();
+  });
+});
