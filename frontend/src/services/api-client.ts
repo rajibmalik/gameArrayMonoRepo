86edd06bbe2ba2a702b76657107150e48c3ad64d
rajibmalik <rajibmalik@hotmail.co.uk>
2024-08-07 16:12:30 +0100

    Updated apiClient to use correct URL

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_RENDER_URL || "http://127.0.0.1:3000/api/v1";
+const baseURL = `${
+  import.meta.env.VITE_RENDER_URL || "http://127.0.0.1:3000"
+}/api/v1`;
 
 export default axios.create({
   baseURL,
